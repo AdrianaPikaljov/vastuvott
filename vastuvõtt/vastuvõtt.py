1d94038261737f15c35db699ca478e1ac169b0d6
adri <pikaljovadri@gmail.com>
2025-05-06 11:59:55 +0300

    Update vastuvõtt.py

--- vastuvõtt/vastuvõtt.py
+++ vastuvõtt/vastuvõtt.py
@@ -1,1 +1,19 @@
 from mmmm import *
+
+def menu():
+    while True:
+        print("\n1. Alusta testi")
+        print("2. Lisa uus küsimus")
+        print("3. Välju")
+
+        valik = input("Vali tegevus (1-3): ")
+        if valik == "1":
+            testi_tegemine()
+        elif valik == "2":
+            lisa_kysimus()
+        elif valik == "3":
+            print("Programmist väljumine.")
+            break
+        else:
+            print("Vale valik. Proovi uuesti.") 
+menu()
